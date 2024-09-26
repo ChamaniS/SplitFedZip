88cc680a50905bbf342c8cc7f9895596a1339ca5
Chamani Shiranthika Jayakody Kankanamalage <csj5@sfu.ca>
2024-09-26 16:29:07 -0700

    Adding files

--- runner.sh
+++ runner.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --nodes=1 
-#SBATCH --gpus-per-node=1     # Request any available GPU
+#SBATCH --gpus-per-node=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8      # There are 32 CPU cores on v100 Cedar GPU nodes
-#SBATCH --mem=32G              # Request the full memory of the node
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
 #SBATCH --time=80:00:00
 
 python cheng_Blasto_F.py   #Change the file name accordingly
